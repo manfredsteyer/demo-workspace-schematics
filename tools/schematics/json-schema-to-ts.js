0e66606a1fd570b2dbf457893abf68de03f0de4e
Manfred Steyer <manfred.steyer@gmx.net>
2020-01-28 20:12:03 +0100

    first commit

--- tools/schematics/json-schema-to-ts.js
+++ tools/schematics/json-schema-to-ts.js
@@ -5,3 +5,7 @@ toTypeScript
     .compileFromFile('tools/schematics/domain/schema.json')
     .then(ts => fs.writeFileSync('tools/schematics/domain/schema.ts', ts));
     
+
+    toTypeScript
+    .compileFromFile('tools/schematics/demo-lib/schema.json')
+    .then(ts => fs.writeFileSync('tools/schematics/demo-lib/schema.ts', ts));
